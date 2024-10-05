ec9e57c3bdc3028e524bab3803b3f4297523be73
Mads Doest <madsdoest@gmail.com>
2024-10-05 23:36:34 +0200

    Updaet lgiht

--- ludum_dare_56/script/spot_light_3d.gd
+++ ludum_dare_56/script/spot_light_3d.gd
@@ -1,8 +1,11 @@
 extends SpotLight3D
 
+@export var min_light = 1.0
+@export var max_light = 500.0
+
 func _on_change_light(change: float) -> void:
 	print("changing light energy")
-	self.light_energy -= change
+	self.light_energy = clamp(self.light_energy - change, min_light, max_light)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
